1811931dd77895c2c01ffc89e402fb9c88e35609
Bufo <bufo24@users.noreply.github.com>
2025-02-08 13:28:00 +0100

    chore: update liquid fee

--- src/libs/liquid/liquid.module.ts
+++ src/libs/liquid/liquid.module.ts
@@ -1,10 +1,8 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 
-import { BoltzRestModule } from '../boltz/boltz.module';
 import { LiquidService } from './liquid.service';
 
 @Module({
-  imports: [forwardRef(() => BoltzRestModule)],
   providers: [LiquidService],
   exports: [LiquidService],
 })
